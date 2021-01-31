7dcd4ae394e8ac663444e64657740a162d51be32
RayenD <zufirka@icloud.com>
2021-02-01 02:14:38 +0300

    Initial Commit

---
+++ ObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjC
+//
+//  Created by Rayen on 01.02.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
